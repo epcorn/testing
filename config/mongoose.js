400895e262617b7a25a768fbbae368c573b3da35
pal-kamlesh <kamleshiit5@gmail.com>
2024-12-09 13:51:04 +0530

    latest

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,16 +1,17 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-dotenv.config(); 
+dotenv.config();
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("MongoDB connected");
-    } catch (error) {
-        console.error("MongoDB connection failed:", error);
-        process.exit(1);
-    }
+  try {
+    console.log(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error);
+    process.exit(1);
+  }
 };
 
 export default connectDB;
