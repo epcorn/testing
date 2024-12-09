3434b8c0653a7ef069f6defb9e16303da3473396
pal-kamlesh <kamleshiit5@gmail.com>
2024-12-09 12:30:26 +0530

    little increment

--- server.js
+++ server.js
@@ -1,9 +1,65 @@
 import express from "express";
 import path from "path";
+import cors from "cors";
 import { fileURLToPath } from "url";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+
 
 const app = express();
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+//Root middleware
+app.use(
+  morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+    ].join(" ");
+  })
+);
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+app.use(fileUpload({ useTempFiles: true }));
+
+app.post("/api/v1/upload", async function (req, res, next) {
+  try {
+    let imageLinks = "";
+    if (req.files) {
+      let images = [];
+      if (req.files.images.length > 0) images = req.files.images;
+      else images.push(req.files.images);
+
+      for (let i = 0; i < images.length; i++) {
+        const result = await cloudinary.uploader.upload(
+          images[i].tempFilePath,
+          {
+            use_filename: true,
+            folder: "employee",
+            quality: 50,
+            resource_type: "auto",
+          }
+        );
+        imageLinks = result.secure_url;
+        fs.unlinkSync(images[i].tempFilePath);
+      }
+      return res
+        .status(200)
+        .json({ message: "Image uploaded Successfully!", link: imageLinks });
+    }
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
 
 (function fn() {
   if (process.env.NODE_ENV === "production") {
